8ccdf6b143b5eb21547307289093a65a1fbbd43f
Your Name <you@example.com>
2022-04-11 22:31:09 -0500

    Inserte un carousel en la sección de productos

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 window.addEventListener("load", function () {
   new Glider(document.querySelector(".carousel_lista"), {
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 2,
+    slidesToScroll: 2,
     // draggable: true,
     dots: ".carousel_indicadores",
     arrows: {
       prev: ".carousel_anterior",
       next: ".carousel_siguiente",
     },
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 768,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: "3",
+          slidesToScroll: "3",
+        },
+      },
+      {
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        // screens greater than >= 1600px
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+    ],
   });
 });
